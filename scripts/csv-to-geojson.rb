6a2a123c35d56fedfe52970e5baf0217defb5079
Jeni Tennison <jeni@theodi.org>
2014-02-22 15:42:27 -0800

    added opening times for dentists

--- scripts/csv-to-geojson.rb
+++ scripts/csv-to-geojson.rb
@@ -4,7 +4,7 @@ require 'json'
 organisationTypes = [
 	# 'CCG',
 	# 'Clinics',
-	# 'Dentists',
+	'Dentists'
 	# 'GP',
 	# 'GPPractices',
 	# 'GSD',
@@ -18,10 +18,26 @@ organisationTypes = [
 	# 'SCP',
 	# 'Trust',
 	# 'WIC'
-	'GSD'
 ]
 
 organisationTypes.each do |organisationType|
+	openingTimes = {}
+	if organisationType == 'Dentists'
+		csv = File.expand_path("../../sources/DentistOpeningTimes.csv", __FILE__)
+		CSV.foreach(csv, encoding:'iso-8859-1:utf-8', col_sep: "\u00AC", headers: :first_row) do |row|
+			orgID = row['OrganisationId']
+			details = {
+				:WeekDay => row['WeekDay'],
+				:Times => row['Times'],
+				:IsOpen => row['IsOpen'] == 'True',
+				:OpeningTimeType => row['OpeningTimeType']
+			}
+			details['AdditionalOpeningDate'] = row['AdditonalOpeningDate'] unless row['AdditonalOpeningDate'].nil?
+			openingTimes[orgID] = [] unless openingTimes[orgID]
+			openingTimes[orgID].push(details)
+		end
+	end
+
 	features = []
 	csv = File.expand_path("../../sources/#{organisationType}.csv", __FILE__)
 	geojson = File.expand_path("../../#{organisationType}.geojson", __FILE__)
@@ -33,6 +49,7 @@ organisationTypes.each do |organisationType|
 					properties[header] = value
 				end
 			end
+			properties['openingTimes'] = openingTimes[row['OrganisationID']]
 			feature = {
 				:type => 'Feature',
 				:id => row['OrganisationID'],
