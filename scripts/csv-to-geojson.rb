0dfb2849075c31e73131d23d26680e213006874f
Jeni Tennison <jeni@theodi.org>
2014-02-22 15:02:15 -0800

    only include features that have coordinates

--- scripts/csv-to-geojson.rb
+++ scripts/csv-to-geojson.rb
@@ -25,22 +25,24 @@ organisationTypes.each do |organisationType|
 	csv = File.expand_path("../../sources/#{organisationType}.csv", __FILE__)
 	geojson = File.expand_path("../../#{organisationType}.geojson", __FILE__)
 	CSV.foreach(csv, encoding:'iso-8859-1:utf-8', col_sep: "\u00AC", headers: :first_row) do |row|
-		properties = {}
-		row.each do |header,value|
-			unless value.nil? || header == 'Longitude' || header == 'Latitude' || header == 'OrganisationID'
-				properties[header] = value
+		unless row['Longitude'].nil?
+			properties = {}
+			row.each do |header,value|
+				unless value.nil? || header == 'Longitude' || header == 'Latitude' || header == 'OrganisationID'
+					properties[header] = value
+				end
 			end
+			feature = {
+				:type => 'Feature',
+				:id => row['OrganisationID'],
+				:geometry => {
+					:type => 'Point',
+					:coordinates => [row['Longitude'].to_f, row['Latitude'].to_f]
+				},
+				:properties => properties
+			}
+			features.push(feature)
 		end
-		feature = {
-			:type => 'Feature',
-			:id => row['OrganisationID'],
-			:geometry => {
-				:type => 'Point',
-				:coordinates => [row['Longitude'].to_f, row['Latitude'].to_f]
-			},
-			:properties => properties
-		}
-		features.push(feature)
 	end
 	collection = {
 		:type => 'FeatureCollection',
